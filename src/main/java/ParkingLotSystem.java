ee970cdc30892c79e66a5245138b12c647a24c06
aashishogale <aashishogale9@gmail.com>
2020-03-21 12:48:52 +0530

    added park and unpark functionality

---
+++ src/main/java/ParkingLotSystem.java
@@ -0,0 +1,18 @@
+
+public class ParkingLotSystem {
+    private Object vehicle;
+
+    public boolean park(Object vehicle) {
+        this.vehicle = vehicle;
+        return true;
+    }
+
+    public boolean unPark(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
+    }
+
+}
