e23dcad58f103b6f1af3aa169a830f140e5b744e
aashishogale <aashishogale9@gmail.com>
2020-03-21 12:56:57 +0530

    check whether parkinglot full or not

---
+++ src/main/java/ParkingLotException.java
@@ -0,0 +1,10 @@
+
+public class ParkingLotException extends Exception {
+
+    public String message;
+
+    public ParkingLotException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
