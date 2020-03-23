5b9977151c5c03e17e74d0a320ffdc810e2d6c59
aashishogale <aashishogale9@gmail.com>
2020-03-23 16:00:54 +0530

    parking lot full

--- src/main/java/ParkingLotException.java
+++ src/main/java/ParkingLotException.java
@@ -1,10 +1,8 @@
+package com.parkinglot;
 
 public class ParkingLotException extends Exception {
 
-    public String message;
-
     public ParkingLotException(String message) {
         super(message);
-        this.message = message;
     }
 }
